98a949681a045aeadd3115bec7e580a4de971bb5
Tim Irmler <zwazel@hotmail.com>
2022-11-24 16:12:43 +0100

    size and stuff

--- src/bin/test_height.rs
+++ src/bin/test_height.rs
@@ -2,9 +2,17 @@ use ds_heightmap::Runner;
 
 fn main() {
     let mut runner = Runner::new();
+    runner.set_width(10);
+    runner.set_height(10);
+    runner.set_depth(10.0);
+    runner.set_rough(0.5);
+
     let output = runner.ds();
 
-    println!("data: {:?}", output.data);
+    println!("data:");
+    for row in output.data {
+        println!("{:?}", row);
+    }
     println!("max: {}", output.max);
     println!("min: {}", output.min);
 }
