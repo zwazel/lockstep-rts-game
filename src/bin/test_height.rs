0efdf8b723f8fa85732eb4bf5eab04dab915dc13
Tim Irmler <zwazel@hotmail.com>
2022-11-24 16:14:58 +0100

    what is roughness

--- src/bin/test_height.rs
+++ src/bin/test_height.rs
@@ -5,7 +5,7 @@ fn main() {
     runner.set_width(10);
     runner.set_height(10);
     runner.set_depth(10.0);
-    runner.set_rough(0.5);
+    runner.set_rough(0.3); // q: what is this? a: the roughness of the terrain, 0.0 is flat, 1.0 is very rough. Default: 1.0.
 
     let output = runner.ds();
 
